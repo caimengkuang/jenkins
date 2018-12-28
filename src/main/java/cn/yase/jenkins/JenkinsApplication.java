549b320b9981cd774c71115be47faed1595f331e
yase <yase@changjinglu.net>
2018-12-28 20:11:26 +0800

    fix

--- src/main/java/cn/yase/jenkins/JenkinsApplication.java
+++ src/main/java/cn/yase/jenkins/JenkinsApplication.java
@@ -2,9 +2,17 @@ package cn.yase.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsApplication {
+public class JenkinsApplication  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(JenkinsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsApplication.class, args);
