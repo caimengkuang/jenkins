75b3305bb9c16f7e3402784032fc31fc986bb75a
yase <yase@changjinglu.net>
2018-12-29 19:56:33 +0800

    fix ： 添加 logback 日志测试

--- src/main/java/cn/yase/jenkins/controller/MainController.java
+++ src/main/java/cn/yase/jenkins/controller/MainController.java
@@ -1,5 +1,7 @@
 package cn.yase.jenkins.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
+    private static Logger logger = LoggerFactory.getLogger(MainController.class);
+
     @GetMapping("/test")
     public String test(String message){
+        logger.info("message : {} "+message);
         return message;
     }
 
